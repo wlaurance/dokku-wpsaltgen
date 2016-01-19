a33f577e67eba0957d4ad600ab458ed18990d0d0
wlaurance <w.laurance@gmail.com>
2016-01-19 15:15:58 -0800

    Print variables

--- main.go
+++ main.go
@@ -4,9 +4,21 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"regexp"
 )
 
+func getAppName() string {
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) < 1 {
+		panic(fmt.Errorf("App Name Not provided"))
+	}
+	arg := argsWithoutProg[0]
+	return arg
+}
+
 func main() {
+	appName := getAppName()
 	resp, err := http.Get("https://api.wordpress.org/secret-key/1.1/salt/")
 	if err != nil {
 		fmt.Println(fmt.Sprintf("Error %s", err.Error()))
@@ -19,5 +31,11 @@ func main() {
 		panic(err)
 	}
 	salts := string(body)
-	fmt.Println(salts)
+	reg := regexp.MustCompile(`'(.+)',\s+'(.+)'`)
+	matches := reg.FindAllStringSubmatch(salts, -1)
+	command := fmt.Sprintf("dokku config:set %s", appName)
+	for _, matches := range matches {
+		command = command + fmt.Sprintf(" %s='%s'", matches[1], matches[2])
+	}
+	fmt.Println(command)
 }
